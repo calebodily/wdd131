b8734bc65ccafa54cd89bfdc7b2eb88a50cfa865
Cale Bodily <cbodily1@byupathway.edu>
2026-07-09 11:25:52 -0600

    media query and bom activities

---
+++ week02/scripts/bom.js
@@ -0,0 +1,20 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", function () {
+    if (input.value != "") {
+        const li = document.createElement("li");
+        li.textContent = input.value;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        deleteBtn.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+        input.value = "";
+    }
+    input.focus();
+});
